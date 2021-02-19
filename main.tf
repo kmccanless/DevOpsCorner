e8e996d97bd8c9d479f35721ac0a2cdd5cd09af9
Keith McCanless <keith@keiths-mbp.lan>
2021-02-19 17:06:08 -0500

    added instance in public subnet

--- main.tf
+++ main.tf
@@ -2,6 +2,19 @@ provider "aws" {
   region = "us-east-2"
   profile = "default"
 }
+data "aws_ami" "amazon-linux-2" {
+  most_recent = true
+  owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-ecs-hvm-*"]
+  }
+}
+
+data "http" "myip" {
+  url = "http://icanhazip.com"
+}
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
@@ -38,3 +51,29 @@ resource "aws_subnet" "private" {
     Name = "private"
   }
 }
+resource aws_key_pair "pub_key"{
+  key_name = "devopscorner"
+  public_key = file("./devopscorner.pub")
+}
+resource "aws_instance" "pub_bastion" {
+  ami = data.aws_ami.amazon-linux-2.id
+  associate_public_ip_address = true
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.pub_key.key_name
+  vpc_security_group_ids = [aws_security_group.pub_sg.id]
+  tags = {
+    Name = "DevOpsCorner-Pub"
+  }
+  subnet_id = aws_subnet.public.id
+}
+resource "aws_security_group" "pub_sg" {
+  name = "devopscorner-pub-sg"
+  vpc_id = aws_vpc.main.id
+  ingress {
+    from_port = 22
+    protocol = "tcp"
+    to_port = 22
+    cidr_blocks = ["${chomp(data.http.myip.body)}/32"]
+  }
+}
+
