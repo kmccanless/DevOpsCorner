56cdfb13646e07a318ecad229c969b548c4d50aa
Keith McCanless <keith@keiths-mbp.lan>
2021-03-05 15:51:23 -0500

    added provisioning examples

--- main.tf
+++ main.tf
@@ -55,17 +55,65 @@ resource aws_key_pair "pub_key"{
   key_name = "devopscorner"
   public_key = file("./devopscorner.pub")
 }
-resource "aws_instance" "pub_bastion" {
+resource "aws_instance" "ansible" {
   ami = "ami-09246ddb00c7c4fef"
   associate_public_ip_address = true
   instance_type = "t2.micro"
   key_name = aws_key_pair.pub_key.key_name
-  vpc_security_group_ids = [aws_security_group.pub_sg.id]
+  vpc_security_group_ids = [
+    aws_security_group.pub_sg.id]
   tags = {
-    Name = "DevOpsCorner-Pub"
+    Name = "DevOpsCorner-Ansible"
   }
   subnet_id = aws_subnet.public.id
+
+  connection {
+    host = self.public_ip
+    type = "ssh"
+    user = "ec2-user"
+    private_key = file("devopscorner")
+  }
+
+  provisioner "file" {
+    source = "./ansible/play.yml"
+    destination = "/tmp/play.yml"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum update -y",
+      "sudo amazon-linux-extras install ansible2 -y",
+      "ansible-playbook /tmp/play.yml"
+    ]
+  }
+  provisioner "file" {
+    source = "./ansible/index.html"
+    destination = "/tmp/index.html"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo mv /tmp/index.html /var/www/html/index.html"
+    ]
+  }
 }
+
+//resource "aws_instance" "user_data" {
+//  ami = "ami-09246ddb00c7c4fef"
+//  associate_public_ip_address = true
+//  instance_type = "t2.micro"
+//  key_name = aws_key_pair.pub_key.key_name
+//  vpc_security_group_ids = [aws_security_group.pub_sg.id]
+//  tags = {
+//    Name = "DevOpsCorner-User-Data"
+//  }
+//  subnet_id = aws_subnet.public.id
+//  user_data = <<EOF
+//    #!/bin/bash
+//    yum install httpd -y
+//    systemctl start httpd
+//    echo "<h1>Let there be provisioning</h1>" | sudo tee /var/www/html/index.html
+//  EOF
+//
+//}
 resource "aws_security_group" "pub_sg" {
   name = "devopscorner-pub-sg"
   vpc_id = aws_vpc.main.id
@@ -75,11 +123,17 @@ resource "aws_security_group" "pub_sg" {
     to_port = 22
     cidr_blocks = ["${chomp(data.http.myip.body)}/32"]
   }
+  ingress {
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+    cidr_blocks = ["${chomp(data.http.myip.body)}/32"]
+  }
   egress {
     from_port = 0
     protocol = "-1"
     to_port = 0
-    cidr_blocks = ["0.0.0.0/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
