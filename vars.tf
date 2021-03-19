41555928e3cd44981ac8bdc3bf681acce35a10da
Jessie Puls <jessie.puls@testdouble.com>
2021-03-19 16:38:26 -0500

    removing unused variable

--- vars.tf
+++ vars.tf
@@ -2,7 +2,3 @@ variable "availibility_zones" {
   type = list(string)
   default = ["us-east-2a","us-east-2b"]
 }
-variable "cidr_blocks" {
-  type = list(string)
-  default = ["10.0.1.0/24","10.0.2.0/24"]
-}
